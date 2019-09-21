fa4c5c3ee888a1a8165af02cbc680d0833c1572b
Thayrone Marques <thayroneprogsi@gmail.com>
2019-09-21 13:33:44 -0300

    ThirdEye - Alterações no layout base aplicação

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage_view, name='homepage_view'),
+    path('index/', views.index_view, name='index_view'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def homepage_view(request):
     return render(request, 'core/homepage.html', {})
 
+def index_view(request):
+    return render(request, 'core/index.html')
+
